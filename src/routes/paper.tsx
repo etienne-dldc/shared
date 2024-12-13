a80155012937a776920d27aff0e6f45bb9a4d9e0
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2024-12-14 00:08:56 +0100

    Paper playground

--- src/routes/paper.tsx
+++ src/routes/paper.tsx
@@ -1,16 +1,27 @@
 import { Variants } from "../playground/Variants";
 import { Paper } from "../shared/components/common/Paper";
 
+const LEVEL_VARIANTS = {
+  background: "background",
+  card: "card",
+  dialog: "dialog",
+  popover: "popover",
+} as const;
+
 export function PaperRoute() {
   return (
     <Variants
       initialAxis={{ row: [], column: [] }}
-      defaultSelected={{}}
-      dimensions={{}}
-      render={() => {
+      dimensions={{
+        level: LEVEL_VARIANTS,
+      }}
+      defaultSelected={{
+        level: "card",
+      }}
+      render={({ level }) => {
         return (
-          <Paper level="popover">
-            <div className="w-12 h-12 bg-blue-300" />
+          <Paper level={level} className="p-4">
+            <div className="w-12 h-12 bg-white/5" />
           </Paper>
         );
       }}
