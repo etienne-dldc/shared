5ab2618438ed49b456d533aa0eae40a9ffd2025e
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2024-12-18 22:22:25 +0100

    Add SearchInput

---
+++ src/shared/components/form/SearchInput.tsx
@@ -0,0 +1,47 @@
+import { Backspace } from "@phosphor-icons/react";
+import { useRef } from "react";
+import { Button } from "../button/Button";
+import { Tooltip } from "../popovers/Tooltip";
+import { TextInput } from "./TextInput";
+
+interface SearchInputProps {
+  value: string;
+  onValueChange: (value: string) => void;
+  className?: string;
+  placeholder?: string;
+}
+
+export function SearchInput({
+  value,
+  onValueChange,
+  ...inputProps
+}: SearchInputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <TextInput
+      value={value}
+      onValueChange={onValueChange}
+      ref={inputRef}
+      endActions={
+        value.length > 0 && (
+          <Tooltip content="Effacer">
+            <Button
+              variant="tertiary"
+              size="xs"
+              rounded="all"
+              className="mr-1.5"
+              color="red"
+              icon={<Backspace />}
+              onClick={() => {
+                onValueChange("");
+                inputRef.current?.focus();
+              }}
+            />
+          </Tooltip>
+        )
+      }
+      {...inputProps}
+    />
+  );
+}
