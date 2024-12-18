1bca312a0b20ca48ac5cfbc0267c56feae66202a
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2024-12-18 22:26:55 +0100

    Add NonEmptyList

---
+++ src/shared/components/common/NonEmptyList.tsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+
+interface NonEmptyListProps<T> {
+  items: T[];
+  render: (item: T, index: number) => React.ReactNode;
+  empty?: React.ReactNode;
+  nonEmptyFooter?: React.ReactNode;
+}
+
+export const NonEmptyList = function NonEmptyList<T>({
+  items,
+  render,
+  empty,
+  nonEmptyFooter,
+}: NonEmptyListProps<T>) {
+  if (items.length === 0) {
+    return <Fragment>{empty}</Fragment>;
+  }
+  return (
+    <Fragment>
+      {items.map(render)}
+      {nonEmptyFooter}
+    </Fragment>
+  );
+};
