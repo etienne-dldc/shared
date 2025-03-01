94d70f1a581520c0e0131970765a9896ead98af9
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-03-01 17:54:09 +0100

    Add onTitleClick

--- src/shared/components/layouts/PageTitle.tsx
+++ src/shared/components/layouts/PageTitle.tsx
@@ -5,19 +5,20 @@ import { IconBox } from "../common/IconBox";
 interface PageTitleProps {
   icon?: React.ReactNode;
   title: React.ReactNode;
+  onTitleClick?: () => void;
   details?: React.ReactNode;
   className?: string;
   startActions?: React.ReactNode;
   endActions?: React.ReactNode;
 }
 
-export function PageTitle({ icon, title, details, className, startActions, endActions }: PageTitleProps) {
+export function PageTitle({ icon, title, onTitleClick, details, className, startActions, endActions }: PageTitleProps) {
   return (
     <div className={cn("flex flex-row items-center gap-2", className)}>
       {startActions && <div className="flex flex-row gap-2">{startActions}</div>}
       <div className="flex flex-row pl-1 gap-2">
         {icon && <IconBox size={28} icon={icon} className="p-0.5" weight="bold" />}
-        <h1 className="text-2xl font-semibold">
+        <h1 className="text-2xl font-semibold" onClick={onTitleClick}>
           {title}
           {isNotNil(details) && (
             <>
